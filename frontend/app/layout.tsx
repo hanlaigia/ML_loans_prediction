8b4f000f36ea0691d5f6e6bcb2916356cbc3e84d
Gia Han <nhahan0908@gmail.com>
2025-11-11 16:51:29 +0700

    Cập nhật code Hân

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Loan Prediction App',
+  description: 'A web application for predicting loan approval and risk level.',
+  generator: 'Mishka Team',
 }
 
 export default function RootLayout({
